ee2a5b870fab3266aa537fc61a314a3791a664fc
jordankomnick <jordan@komnick.com>
2022-10-22 12:06:52 -0400

    Add Mongo connectivity and POST endpoint

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 Prepared by Jordan Komnick
 
 ## Installation
+### Prerequisites
+Exact installation and setup instructions will vary depending on your operating system and version, see linked websites for details
+- NodeJS 16: https://nodejs.org/en/download/
+- MongoDB Community Server: https://www.mongodb.com/try/download/community
+  - Create a Mongo database called "local" with a collection called "posts" running on the default port 27017
 
 ## Running
+1. `npm install`
+2. `npm run start`
 
 ## Caveats
 - I am working on a laptop from 2014 with 8GB RAM, so I could not set up Docker for this project
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,46 @@
 import express from "express";
+import bodyParser from 'body-parser'
+ // default port to listen
+import {MongoClient} from "mongodb";
+
 const app = express();
-const port = 8080; // default port to listen
+const port = 8080;
+
+app.use(bodyParser.json())
 
 // define a route handler for the default home page
 app.get( "/", (req, res ) => {
     res.send( "Hello world!" );
 } );
+
+app.post('/', async (req, res) => {
+    const {content, user_id} = req.body;
+    const postData = {
+        content,
+        created_time: new Date(),
+        updated_time: new Date(),
+        user_id
+    }
+
+    let result;
+
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        const db = client.db("local");
+        const postsCollection = db.collection("posts");
+        result = await postsCollection.insertOne(postData);
+    } finally {
+        await client.close();
+    }
+
+    if(result) {
+        res.send(result.insertedId);
+    } else {
+        res.send('Error!');
+    }
+})
 
 // start the Express server
 app.listen( port, () => {
