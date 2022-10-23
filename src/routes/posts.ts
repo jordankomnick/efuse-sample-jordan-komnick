26ca71e513da22bdb55a9314a21d9bda706f55be
jordankomnick <jordan@komnick.com>
2022-10-23 12:53:11 -0400

    Add get all posts and get post by id routes

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,7 +1,65 @@
 import * as express from "express";
-import {MongoClient} from "mongodb";
+import {Document, MongoClient, ObjectId, WithId} from "mongodb";
 
-export const register = ( app: express.Application ) => {
+const uri = "mongodb://localhost:27017";
+
+export const register = (app: express.Application) => {
+    app.get('/', async (req, res) => {
+        const result: WithId<Document>[] = [];
+        let errorMessage;
+
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const db = client.db("local");
+            const postsCollection = db.collection("posts");
+            const cursor = await postsCollection.find();
+            await cursor.forEach((doc) => {
+                result.push(doc);
+            })
+        } catch(e) {
+            errorMessage = e.message;
+        } finally {
+            await client.close();
+        }
+
+        if(errorMessage) {
+            res.status(500).send({message: errorMessage});
+        } else {
+            res.status(200).send(result);
+        }
+    })
+
+    app.get('/:id', async (req, res) => {
+        const{id} = req.params;
+
+        let result;
+        let errorMessage;
+
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const db = client.db("local");
+            const postsCollection = db.collection("posts");
+            result = await postsCollection.findOne({'_id': new ObjectId(id)});
+        } catch(e) {
+            errorMessage = e.message;
+        } finally {
+            await client.close();
+        }
+
+        if(errorMessage) {
+            res.status(500).send({message: errorMessage});
+        }
+
+        if(result) {
+            res.status(200).send(result);
+        } else {
+            res.status(404).send({message: `No post found with id ${id}`});
+        }
+
+    })
+
     app.post('/', async (req, res) => {
         const {content, user_id} = req.body;
         const postData = {
@@ -14,7 +72,6 @@ export const register = ( app: express.Application ) => {
         let result;
         let errorMessage;
 
-        const uri = "mongodb://localhost:27017";
         const client = new MongoClient(uri);
         try {
             await client.connect();
