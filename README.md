cf1b0faaecf0b7942be8c7efbe6b37d300026b9b
jordankomnick <jordan@komnick.com>
2022-10-23 20:45:37 -0400

    Add update and delete endpoints, update readme

--- README.md
+++ README.md
@@ -8,9 +8,23 @@ Exact installation and setup instructions will vary depending on your operating 
 - MongoDB Community Server: https://www.mongodb.com/try/download/community
   - Create a Mongo database called "local" with a collection called "posts" running on the default port 27017
 
-## Running
-1. `npm install`
-2. `npm run start`
+### Main Components
+Run the following in the directory where you have downloaded the project:
+
+`npm install`
+
+## Running and Usage
+Once everything is installed, run the application with:
+
+`npm run start`
+
+There will be a console log with the port number when the server is running locally. Endpoints can be hit via curl or a free tool such as Postman or Insomnia.
+
+## Design and Implementation Decisions
+- Despite Redis connectivity being in the "definition of done" for phase 0, I didn't implement it in that phase. This is because I looked ahead and saw that Redis was not required until phase 4 when caching is introduced. In real projects, I generally don't decide on and wire up major new dependencies until right before they are needed, for the following reasons:
+  - You want to wait until you have as much information as possible before deciding on a dependency, especially if it is a decision that is hard to reverse
+  - It is better to spend developer time working on features that are immediately valuable rather than laying groundwork for potential future development, especially on tight deadlines
+- I am assuming that whenever a post is updated, the user_id will always be passed in, as it would not make sense to update a post without a user performing the action
 
 ## Caveats
 - I am working on a laptop from 2014 with 8GB RAM, so I could not set up Docker for this project
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -90,4 +90,80 @@ export const register = (app: express.Application) => {
             res.status(500).send({message: errorMessage});
         }
     })
+
+    app.put('/:id', async(req, res) => {
+        const {id} = req.params;
+        const {content, user_id} = req.body;
+
+        const update = {
+            $set: {
+                content,
+                user_id,
+                updated_time: new Date()
+            }
+        }
+
+        let result;
+        let errorMessage;
+
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const db = client.db("local");
+            const postsCollection = db.collection("posts");
+            const found = await postsCollection.findOne({'_id': new ObjectId(id)});
+
+            if(!found) {
+                res.status(404).send({message: `No post found with id ${id}`});
+            }
+
+            await postsCollection.updateOne({'_id': new ObjectId(id)}, update);
+            result = await postsCollection.findOne({'_id': new ObjectId(id)});
+        } catch(e) {
+            errorMessage = e.message;
+        } finally {
+            await client.close();
+        }
+
+        if(result) {
+            res.status(200).send({id: result});
+        } else {
+            res.status(500).send({message: errorMessage});
+        }
+    })
+
+    app.delete('/:id', async(req, res) => {
+        const {id} = req.params;
+
+        let result;
+        let toBeDeleted = null;
+        let errorMessage;
+
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            const db = client.db("local");
+            const postsCollection = db.collection("posts");
+            toBeDeleted = await postsCollection.findOne({'_id': new ObjectId(id)});
+            //TODO: fix error handling, apparently this doesn't return
+            if(!toBeDeleted) {
+                res.status(404).send({message: `No post found with id ${id}`});
+            }
+            result = await postsCollection.deleteOne({'_id': new ObjectId(id)});
+        } catch(e) {
+            errorMessage = e.message;
+        } finally {
+            await client.close();
+        }
+
+        if(errorMessage) {
+            res.status(500).send({message: errorMessage});
+        }
+
+        if(result && result.deletedCount > 0) {
+            res.status(200).send(toBeDeleted);
+        } else {
+            res.status(404).send({message: `No post found with id ${id}`});
+        }
+    })
 };
