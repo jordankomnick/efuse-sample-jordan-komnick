b74d53659e9713f2f1753f4d1190357a191f762d
jordankomnick <jordan@komnick.com>
2022-10-24 18:50:19 -0400

    Refactor posts routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import bodyParser from 'body-parser'
-import * as postsRoutes from './routes/posts.routes';
+import postsRoutes from './routes/posts.routes';
 
 const port = 8080;
 
 const app = express();
 
 app.use(bodyParser.json())
-postsRoutes.register(app);
+app.use('/posts', postsRoutes);
 
 // start the Express server
 app.listen( port, () => {
